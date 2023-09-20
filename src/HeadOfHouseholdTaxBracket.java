51f011adc4ed0964f0d4b360a013b7e072f064ef
JamarianS <jstaimee@ggc.edu>
2023-09-19 20:02:06 -0400

    updated

--- src/HeadOfHouseholdTaxBracket.java
+++ src/HeadOfHouseholdTaxBracket.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class HeadOfHouseholdTaxBracket {
+class HeadOfHouseholdTaxBracket extends TaxBracket {
+    public HeadOfHouseholdTaxBracket() {
+        super(new double[]{15_700, 59_850, 95_350, 182_100, 231_250, 578_100},
+                new double[]{0.10, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37});
+    }
 }
